d52cb7a2a6692584968aa8bcf33191627dd228a0
Alexy Cruz <ACruz@uvltd.com>
2024-09-17 18:19:35 -0600

    create book fix

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -1,6 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
-import { colors } from './colors';
 import { tokens } from './tokens';
+import { colors } from './colors';
 
 export const GlobalStyle = createGlobalStyle`
   @font-face {
@@ -15,17 +15,50 @@ export const GlobalStyle = createGlobalStyle`
   }
 
   body {
-    font-family: 'KidsFont', sans-serif;
+    font-family: 'KidsFont', sans-serif !important;
     font-size: ${tokens.fontSize.base};
     background-image: url('/images/kids-background.jpg');
     background-size: cover;
     background-attachment: fixed;
     background-position: center;
     min-height: 100vh;
+    color: ${colors.text};
   }
 
-  button, input, textarea, select, .ui.pagination.menu, .ui.pagination.menu .item {
+  h1, h2, h3, h4, h5, h6, .ui.header {
     font-family: 'KidsFont', sans-serif !important;
-    font-size: ${tokens.fontSize.base};
+    color: ${colors.primary} !important;
+  }
+
+  button, input, textarea, select, .ui.button, .ui.input > input, .ui.dropdown, .ui.menu, .ui.label {
+    font-family: 'KidsFont', sans-serif !important;
+  }
+
+  .ui.card > .content > .header {
+    font-family: 'KidsFont', sans-serif !important;
+    color: ${colors.primary} !important;
+  }
+
+  .ui.card > .content > .description {
+    color: ${colors.text} !important;
+  }
+
+  .ui.card > .extra {
+    color: ${colors.lightText} !important;
+  }
+
+  .ui.pagination.menu .item {
+    font-family: 'KidsFont', sans-serif !important;
+  }
+
+  .ui.message {
+    font-family: 'KidsFont', sans-serif !important;
+  }
+
+  a {
+    color: ${colors.secondary};
+    &:hover {
+      color: ${colors.primary};
+    }
   }
 `;
